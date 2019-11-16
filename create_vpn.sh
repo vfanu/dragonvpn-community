2ca36963a09895a02a2ceb64e8d4fbc97da3ceba
Chris <chris@pop-os.localdomain>
2019-11-16 23:39:07 +0000

    added email params

--- create_vpn.sh
+++ create_vpn.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "Creating VPN"
+
+if [[ -n $1 ]]; then
+
+  sed -i 's/_enter_email_address_/'$1'/g' foehammer-vpn/main.tf
+
+fi
+
 cd foehammer-vpn
 terraform init -input=false
 terraform apply -input=false -auto-approve
