a9ae5c21ee5fde1069967b90a28ebdfb5db625ee
Chris <chris@pop-os.localdomain>
2019-12-01 00:54:18 +0000

    Added region info

--- create_vpn.sh
+++ create_vpn.sh
@@ -2,6 +2,10 @@
 
 echo "Creating VPN"
 
+rm ~/.aws/config
+echo "[default]" >> ~/.aws/config
+echo "eu-west-1" >> ~/.aws/config
+
 if [[ -n $1 ]]; then
 
   sed -i 's/_enter_email_address_/'$1'/g' foehammer-vpn/main.tf
--- destroy_vpn.sh
+++ destroy_vpn.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 echo "Destroying VPN"
-
 echo "Pick it up!"
+
+rm ~/.aws/config
+echo "[default]" >> ~/.aws/config
+echo "eu-west-1" >> ~/.aws/config
 
 cd foehammer-vpn
 terraform destroy -input=false -auto-approve
