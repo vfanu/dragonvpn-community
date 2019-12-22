fb0abaafdab92c5d8d276cf14ba892da13a33b24
Chris <chris@pop-os.localdomain>
2019-12-22 21:22:29 +0000

    Added params for region

--- destroy_vpn.sh
+++ destroy_vpn.sh
@@ -5,7 +5,7 @@ echo "Pick it up!"
 
 rm ~/.aws/config
 echo "[default]" >> ~/.aws/config
-echo "eu-west-1" >> ~/.aws/config
+echo $1 >> ~/.aws/config
 
 cd foehammer-vpn
 terraform destroy -input=false -auto-approve
